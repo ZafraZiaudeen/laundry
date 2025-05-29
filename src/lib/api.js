a1789a59e33563c5877736b87f1ce21aa6054558
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-05-30 02:17:39 +0530

    made some changes in main.jsx

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const Backend_URL = "http://localhost:8000";
+const Backend_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const api = createApi({
   reducerPath: "api",
